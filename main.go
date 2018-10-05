a1e5f8921c3c4f0af93722e8a2d7cebdf138925b
Xiangyang Li <13622004447@163.com>
2018-10-05 12:59:32 +0800

    add log settings

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	_ "hostskeeper/routers"
 )
 
 func main() {
+	logs.EnableFuncCallDepth(true)
+	logs.SetLogFuncCallDepth(3)
 	beego.Run()
 }
